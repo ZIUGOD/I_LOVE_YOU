ddec19f3a87344284ec36c6424fa9835db26c6e5
ZIUGOD <caique.dubstep@gmail.com>
2023-07-08 14:50:50 -0300

    Ela disse SIM!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
+const body = window.document.getElementById("body");
 const title = window.document.getElementById("title");
 const caption = window.document.getElementById("caption");
 const date_html = window.document.getElementById("date");
 const now = new Date();
-const week = ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"];
-const month = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
+const week = [
+    "domingo", "segunda-feira",
+    "terça-feira", "quarta-feira",
+    "quinta-feira", "sexta-feira",
+    "sábado",
+];
+const month = [
+    "janeiro", "fevereiro", "março", "abril",
+    "maio", "junho", "julho", "agosto",
+    "setembro", "outubro", "novembro", "dezembro",
+];
 
 function actual_date() {
     let actual_day = now.getDate();
@@ -32,18 +42,37 @@ function actual_date() {
 
     setTimeout(() => {
         she_said_yes_at = `No dia ${actual_day} de ${actual_month} de ${actual_year}, ${week[now.getDay()]},\n 
-        às ${actual_hour}:${actual_minutes}:${actual_seconds}, ela disse SIM!`;
+        às ${actual_hour}h ${actual_minutes}m ${actual_seconds}s, ela disse SIM!`;
 
         date_html.innerText = she_said_yes_at;
 
         console.log(she_said_yes_at);
-    }, 5000);
+    }, 41000);
 }
 
 
 function random_int(min, max) {
+    if (max < min) {
+        [max, min] = [min, max];
+    }
+
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function add_img() {
+    const image = new Image(64, 64); // Largura (width) e altura (height).
+    image.src = "assets/images/heart_gif.gif";
+
+    image.classList.add("heart-image");
+
+    image.style.position = "absolute";
+    image.style.opacity = `0.${random_int(1, 100)}`
+    image.style.left = String(random_int(10, 90)) + "vw";
+    image.style.top = String(random_int(10, 90)) + "vh";
+
+    body.append(image);
+}
+
 
 let container_buttons = window.document.getElementById("container-buttons");
 
@@ -56,12 +85,12 @@ btn_no.innerHTML = "Não";
 btn_no.id = "btn-no";
 btn_no.onmouseover = () => {
     btn_no.style.position = "absolute";
-    btn_no.style.left = String(random_int(5, 88)) + "vw";
-    btn_no.style.top = String(random_int(12, 34)) + "vw";
+    btn_no.style.left = `${random_int(5, 88)}vw`;
+    btn_no.style.top = `${random_int(22, 66)}vh`;
 };
 btn_no.onclick = () => {
     btn_no.style.left = String(random_int(5, 88)) + "vw";
-    btn_no.style.top = String(random_int(12, 34)) + "vw";
+    btn_no.style.top = String(random_int(22, 66)) + "vh";
 };
 
 btn_yes.type = "button";
@@ -79,11 +108,22 @@ btn_yes.onclick = () => {
         caption.innerText = `Prometo te fazer a mulher mais feliz do mundo!!!`
     }, 1000);
 
+
+    setTimeout(() => {
+        for (let i = 0; i < 15; i++) {
+            setTimeout(add_img, i * 300);
+        }
+    }, 5000);
+
+
     setTimeout(() => {
         window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    }, 12500);
+
+    }, 30000);
+
     actual_date();
 };
+
 
 setTimeout(() => {
     title.innerText = "Karolayne...";
@@ -96,4 +136,5 @@ setTimeout(() => {
             container_buttons.appendChild(btn_no);
         }, 1750);
     }, 2500);
-}, 1250);
+}, 1300);
+
